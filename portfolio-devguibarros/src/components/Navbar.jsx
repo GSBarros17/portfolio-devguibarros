bc367955f12a0d815ed836b68f1f0c81d0fee1ae
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-28 11:16:50 -0300

    add menuburguer e img do buttonlight e dev navbar

--- portfolio-devguibarros/src/components/Navbar.jsx
+++ portfolio-devguibarros/src/components/Navbar.jsx
@@ -1,10 +1,30 @@
+import styles from "./Navbar.module.css"
 import ImgLogo from "../img/logo.png"
+import ImgDayNight from "../img/dia-e-noite.png"
+import { IoMenu } from "react-icons/io5";
+
 
 export default function Navbar(){
     return(
-        <div>
-            <img src={ImgLogo} alt="Logo"/>
-            <h1>Navbar</h1>
+        <div className={styles.navbarContainer}>
+            <div>
+                <button>
+                    <IoMenu/>
+                </button>
+                <img src={ImgLogo} alt="Logo"/>
+                <button>
+                    <img src={ImgDayNight} alt=""/>
+                </button>
+            </div>
+            <nav>
+                <ul>
+                    <li>Sobre mim</li>
+                    <li>Hard skill</li>
+                    <li>Soft skill</li>
+                    <li>Projetos</li>
+                    <li>Contatos</li>
+                </ul>
+            </nav>
         </div>
     )
 }
