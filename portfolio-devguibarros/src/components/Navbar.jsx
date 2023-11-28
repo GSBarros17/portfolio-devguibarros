df6af85c23f79549dda3f466fee0144570782fa3
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-28 11:02:59 -0300

    add pg de img e logos do projeto

--- portfolio-devguibarros/src/components/Navbar.jsx
+++ portfolio-devguibarros/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-
+import ImgLogo from "../img/logo.png"
 
 export default function Navbar(){
     return(
         <div>
+            <img src={ImgLogo} alt="Logo"/>
             <h1>Navbar</h1>
         </div>
     )
