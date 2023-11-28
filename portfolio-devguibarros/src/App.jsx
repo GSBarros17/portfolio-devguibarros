404de352c2d886bda47540fc851092a20ca232b0
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-28 17:12:22 -0300

    add o react router dom ao projeto em app

--- portfolio-devguibarros/src/App.jsx
+++ portfolio-devguibarros/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from "./page/Home/Home"
 
 export default function App() {
   return (
-    <div>
-      <Navbar/>
-      <Home/>
+    <div className="container">
+      <BrowserRouter>
+        <Navbar>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+            </Routes>
+          </div>
+        </Navbar>
+      </BrowserRouter>
     </div>
   )
 }
