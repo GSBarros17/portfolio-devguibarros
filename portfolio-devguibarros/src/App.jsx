ea0cc0f3b841b5e53307c59eca540e238a1c652b
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-28 10:54:49 -0300

    criando Navbar e seu estilo

--- portfolio-devguibarros/src/App.jsx
+++ portfolio-devguibarros/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 import Home from "./page/Home/Home"
 
 export default function App() {
   return (
     <div>
+      <Navbar/>
       <Home/>
     </div>
   )
