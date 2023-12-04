db2eeb507f43f773af42323b961ce949bda92d2c
Guilherme Barros <gsbguilherme23@gmail.com>
2023-12-04 15:40:16 -0300

    dev a div aboutme da pg home

--- portfolio-devguibarros/src/page/Home/Home.jsx
+++ portfolio-devguibarros/src/page/Home/Home.jsx
@@ -1,9 +1,22 @@
+import DevImg from "../../img/devimg.jpg"
 import styles from "./Home.module.css"
 
 export default function Home(){
     return(
         <div className={styles.homeContainer}>
-            <h1>Sobre Mim</h1>
+            <div className={styles.aboutMe}>
+                <img src={DevImg} alt="Foto digital do desenvolvedor"/>
+                <div className={styles.aboutMeInfo}>
+                    <h1>Sobre Mim</h1>
+                    <p>
+                        Olá, me chamo Guilherme Soares de Barros, trabalhei mais de 7 anos como bancário porém,
+                        encontrei no setor de tecnologia a minha paixão. Atualmente venho trabalhando em projetos
+                        pessoais para desenvolver um portfólio de apresentação. tenho como hobbies estudar novas
+                        tecnologias, praticar esportes, escutar musica e viajar.
+                    </p>
+                </div>
+            </div>
+            
         </div>
     )
 }
