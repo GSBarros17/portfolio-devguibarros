e3ac4453027163bda4512d86bbd13f855a658123
Guilherme Barros <gsbguilherme23@gmail.com>
2023-12-05 16:52:33 -0300

    estilizando o projectcard

--- portfolio-devguibarros/src/components/ProjectCard.jsx
+++ portfolio-devguibarros/src/components/ProjectCard.jsx
@@ -1,13 +1,21 @@
 import {PropTypes} from "prop-types"
+import styles from "./ProjectCard.module.css"
+import { IoLogoGithub, IoGlobeOutline } from "react-icons/io5";
 
 export default function ProjectCard({card}){
     return(
-        <div>
-            <h5>{card.title}</h5>
+        <div className={styles.projectCard}>
+            <h4>{card.title}</h4>
             <img src={card.image} alt={card.title}/>
-            <div>
-                <a href={card.urlWeb}>Link web</a>
-                <a href={card.urlGitHub}>GitHub</a>
+            <div className={styles.projectCardActions}>
+                <a href={card.urlWeb}>
+                    <IoGlobeOutline />
+                    Link web
+                </a>
+                <a href={card.urlGitHub}>
+                    <IoLogoGithub />
+                    GitHub
+                </a>
             </div>
         </div>
     )
