77fc207c4aca0242b93ce1f6182e146d826ef328
Guilherme Barros <gsbguilherme23@gmail.com>
2023-12-05 11:16:12 -0300

    fazendo a chamada das imagens em iconsHardSkills

--- portfolio-devguibarros/src/components/iconsHardSkills.jsx
+++ portfolio-devguibarros/src/components/iconsHardSkills.jsx
@@ -1,6 +1,12 @@
 import Html from "../img/html5-icon.png"
 import Css from "../img/css-icon.png"
-
+import Js from "../img/js-icon.png"
+import BootStrap from "../img/bootstrap-icon.png"
+import Vitejs from "../img/vite-js-icon.png"
+import React from "../img/react-icon.png"
+import Nodejs from "../img/nodejs-icon.png"
+import Github from "../img/github-icon.png"
+import Figma from "../img/figma-icon.png"
 
 export const iconsHardSkills = [
     {
