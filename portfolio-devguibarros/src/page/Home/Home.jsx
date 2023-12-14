3ed65f5660f4f35e1c98340158fe39181ff45d74
Guilherme Barros <gsbguilherme23@gmail.com>
2023-12-14 08:52:07 -0300

    app mediaquery em skill e projects

--- portfolio-devguibarros/src/page/Home/Home.jsx
+++ portfolio-devguibarros/src/page/Home/Home.jsx
@@ -50,21 +50,23 @@ export default function Home(){
                     </div>
                 </div>
             </div>
-            <hr className={isLightMode ? styles.light : ""}/>
-            <div id="hard-skill" className={styles.hardSkills}>
-                <h1>Hard Skill</h1>
-                <div className={styles.iconsHardSkills}>
-                   {iconsHardSkills.map(createdIconsHardSkills)}
-                </div> 
+            <hr className={`${styles.lineLightHome}${isLightMode ? styles.light : ""}`}/>
+            <div className={styles.skillsComponent}>
+                <div id="hard-skill" className={styles.hardSkills}>
+                    <h1>Hard Skill</h1>
+                    <div className={styles.iconsHardSkills}>
+                    {iconsHardSkills.map(createdIconsHardSkills)}
+                    </div> 
+                </div>
+                <hr className={`${styles.lineLightHome}${isLightMode ? styles.light : ""}`}/>
+                <div id="soft-skill" className={styles.softSkills}>
+                    <h1>Soft Skills</h1>
+                    <p>
+                        Facilidade de aprendizagem, boa comunicação, criativo, dinâmico, pró ativo, fácil convívio. Procuro sempre trabalhar da melhor forma possível, objetivando o aprendizado constante de novas ferramentas para construir um conhecimento sólido.
+                    </p>
+                </div>
             </div>
-            <hr className={isLightMode ? styles.light : ""}/>
-            <div id="soft-skill" className={styles.softSkills}>
-                <h1>Soft Skills</h1>
-                <p>
-                    Facilidade de aprendizagem, boa comunicação, criativo, dinâmico, pró ativo, fácil convívio. Procuro sempre trabalhar da melhor forma possível, objetivando o aprendizado constante de novas ferramentas para construir um conhecimento sólido.
-                </p>
-            </div>
-            <hr className={isLightMode ? styles.light : ""} />
+            <hr className={`${styles.lineLightHome}${isLightMode ? styles.light : ""}`} />
             <div id="projects" className={styles.projects}>
                 <h1 >Projetos</h1>
                 <div className={styles.projectsCards}>
