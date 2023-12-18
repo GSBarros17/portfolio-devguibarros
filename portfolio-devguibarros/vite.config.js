79b5ab04b9cca1fcbd0bfe61ae174277231cb2bd
Guilherme Barros <gsbguilherme23@gmail.com>
2023-12-18 10:09:28 -0300

    retornando a base de vite.config

--- portfolio-devguibarros/vite.config.js
+++ portfolio-devguibarros/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://subtle-sunburst-5efa1a.netlify.app/portfolio-devguibarros/",
+  base: "/portfolio-devguibarros/",
 })
